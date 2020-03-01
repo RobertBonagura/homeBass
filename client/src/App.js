a3602e46ff9a6fb639f935d30d967cf920defe48
Robert <robert.bonagura@gmail.com>
2020-02-29 20:02:35 -0500

    Rename client and backend folders

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FormContainer from "./FormContainer.js";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h3>React Form</h3>
+        <FormContainer />
+     </div>
+    );
+  }
+}
+export default App;
---
+++ client/src/Input.js
@@ -0,0 +1,18 @@
+const Input = (props) => {
+      
+  <div className="form-group">
+    <label className="form-label">{props.title}</label>
+    <input
+      className="form-input"
+      id={props.name}
+      name={props.name}
+      type={props.type}
+      value={props.value}
+      handleChange={props.onChange}
+      placeholder={props.placeholder} 
+    />
+  </div>
+  
+}
+
+export default Input;

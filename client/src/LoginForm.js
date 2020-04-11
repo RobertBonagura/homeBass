7fa8852d2fbb023364d7a23669f4ba352e28b265
Robert <robert.bonagura@gmail.com>
2020-04-11 09:42:06 -0400

    Allow for login form input fields to be independent of each other

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -5,7 +5,7 @@ class FormContainer extends React.Component {
     constructor(props){
         super(props);
         console.log(this.props);
-        this.state = {value: ''};
+        this.state = {username: '', password: ''};
 
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -16,7 +16,9 @@ class FormContainer extends React.Component {
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+      const value = event.target.value;
+      this.setState({
+        [event.target.name]: event.target.value});
     }
 
     render() {
@@ -24,15 +26,15 @@ class FormContainer extends React.Component {
           <form className="form" onSubmit={this.handleFormSubmit}>
             <h3>Please Login</h3>
             <Input title="Username:"
-              name="name"
+              name="username"
               type="text"
-              value={this.state.value}
+              value={this.state.username}
               onChange={this.handleChange}
               placeholder="Enter your username here"/>
             <Input title="Password:"
-              name="name"
+              name="password"
               type="text"
-              value={this.state.value}
+              value={this.state.password}
               onChange={this.handleChange}
               placeholder="Enter your password here"/>
             <input className="button" type="submit" value="Submit" />
